b8a59b1483072ede1cadd8049f6818aa23aa2683
Agustin Taibo <taibocruz@gmail.com>
2026-05-03 01:00:56 -0300

    add Grafo tests

---
+++ test/logica/modelo/GrafoTest.java
@@ -0,0 +1,31 @@
+package logica.modelo;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.Arrays;
+
+public class GrafoTest {
+
+    private final Localidad buenosAires = new Localidad("Buenos Aires", "Buenos Aires", -34.6, -58.4);
+    private final Localidad rosario = new Localidad("Rosario", "Santa Fe", -32.9, -60.7);
+    private final Localidad cordoba = new Localidad("Córdoba", "Córdoba", -31.4, -64.2);
+
+    @Test
+    public void seCreaCorrectamenteConDosLocalidades() {
+        Arista a = new Arista(buenosAires, rosario, 1000.0);
+        Grafo g = new Grafo(Arrays.asList(buenosAires, rosario), Arrays.asList(a));
+        assertEquals(2, g.getLocalidades().size());
+        assertEquals(1, g.getAristas().size());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void menosDeDosLocalidadesLanzaExcepcion() {
+        new Grafo(Arrays.asList(buenosAires), Arrays.asList());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void grafoNoCompletoLanzaExcepcion() {
+        Arista a = new Arista(buenosAires, rosario, 1000.0);
+        new Grafo(Arrays.asList(buenosAires, rosario, cordoba), Arrays.asList(a));
+    }
+}
