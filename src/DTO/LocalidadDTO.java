ca7cde4868391695e5f9cd7abfb6ecbf93254456
MarceloAgus <agustin.boca10@hotmail.com>
2026-05-06 17:01:06 -0300

    Agregando Clase PersistenciaENJson y LocalidadDTO

---
+++ src/DTO/LocalidadDTO.java
@@ -0,0 +1,14 @@
+package DTO;
+
+public class LocalidadDTO {
+	
+	public String nombre;
+	public String provincia;
+    public double latitud;
+    public double longitud;
+
+
+
+    // constructor vacío (importante para Gson)
+    public LocalidadDTO() {}
+}
