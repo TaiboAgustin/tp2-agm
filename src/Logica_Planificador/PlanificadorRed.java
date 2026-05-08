99c6287ffa9b35ef9d6683cb3744ee93ad45e460
MarceloAgus <devAgus.g@hotmail.com>
2026-05-08 03:54:07 -0300

    PLanificador Conecta UI---------Logica

---
+++ src/Logica_Planificador/PlanificadorRed.java
@@ -0,0 +1,49 @@
+package Logica_Planificador;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import DTO.PersistenciaENJson;
+import logica.modelo.Localidad;
+
+public class PlanificadorRed {
+
+    private static List<Localidad> localidades;
+
+    public PlanificadorRed() {
+        localidades = new ArrayList<>();
+    }
+
+    public void agregarLocalidad(
+            String nombre,
+            String provincia,
+            double latitud,
+            double longitud) {
+
+        Localidad loc = new Localidad(
+                nombre,
+                provincia,
+                latitud,
+                longitud
+        );
+
+        localidades.add(loc);
+    }
+
+    public void guardarDatos() {
+        PersistenciaENJson.guardarLocalidades(
+                localidades,
+                "localidades_test.json"
+        );
+    }
+
+    public void cargarDatos() {
+        localidades = PersistenciaENJson.cargarLocalidades(
+                "localidades_test.json"
+        );
+    }
+
+    public static List<Localidad> getLocalidades() {
+        return localidades;
+    }
+}
